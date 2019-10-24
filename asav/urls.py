ad1c57800fa01143ba2dbd6a8f82a76077116f30
Richard Wohlbold <rigewo02@gmail.com>
2019-10-24 15:48:12 +0200

    Include verwaultung URLs at root

--- asav/urls.py
+++ asav/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('lehrer/', include('verwaltung.urls'))
+    path('', include('verwaltung.urls'))
 ]
