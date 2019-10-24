684d2c760d9e9216abcbf69f0c374835094e3a33
Richard Wohlbold <rigewo02@gmail.com>
2019-10-24 16:31:32 +0200

    Reorganize URLs to match view and template names

--- verwaltung/urls.py
+++ verwaltung/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('erfassung', views.erfassung, name='erfassung'),
     path('auswertung', views.auswertung, name='auswertung'),
-    path('schueler', views.liste, name='schueler_liste'),
-    path('schueler/<int:schueler_id>', views.detail, name='schueler_detail'),
+    path('auswertung/<int:schueler_id>', views.auswertung_detail, name='auswertung_detail'),
     path('impressum', views.impressum, name='impressum'),
 ]
--- verwaltung/views.py
+++ verwaltung/views.py
@@ -7,14 +7,14 @@ from .models import Schueler, AktivitaetErgebnis, Teilnahme
 def index(request):
     return render(request, 'verwaltung/index.html')
 
+def erfassung(request):
+    return render(request, 'verwaltung/erfassung.html')
+
 def auswertung(request):
-    return render(request, 'verwaltung/auswertung.html')
-
-def liste(request):
     s = Schueler.objects.all()
-    return render(request, 'verwaltung/liste.html', {'schueler': s})
+    return render(request, 'verwaltung/auswertung.html', {'schueler': s})
 
-def detail(request, schueler_id):
+def auswertung_detail(request, schueler_id):
     s = get_object_or_404(Schueler, id=schueler_id)
     teilnahmen = Schueler.objects.get(id=schueler_id).teilnahmen.select_related('aktivitaetergebnis', 'aktivitaetergebnis__ergebnis').all()
     return render(request, 'verwaltung/detail.html', {'schueler': s, 'teilnahmen': teilnahmen})
