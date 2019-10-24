b7f4758119173eddf621f516b2dad3db70aa514f
Richard Wohlbold <rigewo02@gmail.com>
2019-10-24 16:16:11 +0200

    Add views and templates for all URLs, rename existing ones

--- verwaltung/urls.py
+++ verwaltung/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('schueler', views.index, name='index'),
-    path('schueler/<int:schueler_id>', views.detail, name='schueler_detail')
+    path('', views.index, name='index'),
+    path('auswertung', views.auswertung, name='auswertung'),
+    path('schueler', views.liste, name='schueler_liste'),
+    path('schueler/<int:schueler_id>', views.detail, name='schueler_detail'),
+    path('impressum', views.impressum, name='impressum'),
 ]
--- verwaltung/views.py
+++ verwaltung/views.py
@@ -4,13 +4,21 @@ from django.shortcuts import render, get_object_or_404
 
 from .models import Schueler, AktivitaetErgebnis, Teilnahme
 
-# Create your views here.
 def index(request):
+    return render(request, 'verwaltung/index.html')
+
+def auswertung(request):
+    return render(request, 'verwaltung/auswertung.html')
+
+def liste(request):
     s = Schueler.objects.all()
-    return render(request, 'verwaltung/index.html', {'schueler': s})
+    return render(request, 'verwaltung/liste.html', {'schueler': s})
 
 def detail(request, schueler_id):
     s = get_object_or_404(Schueler, id=schueler_id)
     teilnahmen = Schueler.objects.get(id=schueler_id).teilnahmen.select_related('aktivitaetergebnis', 'aktivitaetergebnis__ergebnis').all()
     return render(request, 'verwaltung/detail.html', {'schueler': s, 'teilnahmen': teilnahmen})
+
+def impressum(request):
+    return render(request, 'verwaltung/impressum.html')
 
