1a97424837007317e8f07419b6038424bf4f16c4
Alexander Sohn <alexander.sohn642@gmail.com>
2019-10-25 12:28:09 +0200

    Add accordion to tab Auswertung to improve overview

--- verwaltung/views.py
+++ verwaltung/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 
 from .models import Schueler
+from .models import Klasse
 
 def index(request):
     return render(request, 'verwaltung/index.html')
@@ -11,8 +12,9 @@ def erfassung(request):
     return render(request, 'verwaltung/erfassung.html')
 
 def auswertung(request):
+    k = Klasse.objects.all()
     s = Schueler.objects.all()
-    return render(request, 'verwaltung/auswertung.html', {'schueler': s})
+    return render(request, 'verwaltung/auswertung.html', {'schueler': s, 'klassen': k})
 
 def auswertung_detail(request, schueler_id):
     s = get_object_or_404(Schueler, id=schueler_id)
